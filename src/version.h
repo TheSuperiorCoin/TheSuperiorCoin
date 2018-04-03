0ec1f1c6a202587db8b68610e1c6c156ecb2d1d7
mps01k <mike@asinetworkhn.com>
2018-04-02 22:59:03 -0600

    Missing files for Version 7

---
+++ src/version.h
@@ -0,0 +1,6 @@
+#pragma once
+
+extern const char* const SUPERIOR_VERSION_TAG;
+extern const char* const SUPERIOR_VERSION;
+extern const char* const SUPERIOR_RELEASE_NAME;
+extern const char* const SUPERIOR_VERSION_FULL;
