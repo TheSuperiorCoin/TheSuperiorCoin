9a68634b5816eb9f3cf8cf63d1e83473238dc854
mps01k <mike@asinetworkhn.com>
2018-04-02 20:48:06 -0600

    V7 More Updates Blockchain

--- src/blockchain_db/blockchain_db.cpp
+++ src/blockchain_db/blockchain_db.cpp
@@ -1,4 +1,6 @@
-// Copyright (c) 2014-2018, The X Project
+// Copyright (c) 2014-2018, TheSuperiorCoin Project
+// Copyright (c) 2014-2017, The Monero Project
+
 // 
 // All rights reserved.
 // 
--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -37,7 +37,7 @@
 
 #define CRYPTONOTE_MAX_BLOCK_NUMBER                     500000000
 #define CRYPTONOTE_MAX_BLOCK_SIZE                       500000000  // block header blob limit, never used!
-#define CRYPTONOTE_GETBLOCKTEMPLATE_MAX_BLOCK_SIZE	    196608 //size of block (bytes) that is the maximum that miners will produce
+#define CRYPTONOTE_GETBLOCKTEMPLATE_MAX_BLOCK_SIZE	196608 //size of block (bytes) that is the maximum that miners will produce
 #define CRYPTONOTE_MAX_TX_SIZE                          1000000000
 #define CRYPTONOTE_PUBLIC_ADDRESS_TEXTBLOB_VER          0
 #define CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW            60
@@ -98,9 +98,8 @@
 #define BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT          10000  //by default, blocks ids count in synchronizing
 #define BLOCKS_SYNCHRONIZING_DEFAULT_COUNT_PRE_V4       100    //by default, blocks count in blocks downloading
 #define BLOCKS_SYNCHRONIZING_DEFAULT_COUNT              20     //by default, blocks count in blocks downloading
-#define CRYPTONOTE_PROTOCOL_HOP_RELAX_COUNT             3      //value of hop, after which we use only announce of new block
 
-#define CRYPTONOTE_MEMPOOL_TX_LIVETIME                    86400 //seconds, one day
+#define CRYPTONOTE_MEMPOOL_TX_LIVETIME                    (86400*3) //seconds, three days
 #define CRYPTONOTE_MEMPOOL_TX_FROM_ALT_BLOCK_LIVETIME     604800 //seconds, one week
 
 #define COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT           1000
@@ -140,9 +139,14 @@
 
 #define HF_VERSION_DYNAMIC_FEE                  4
 #define HF_VERSION_MIN_MIXIN_4                  6
+#define HF_VERSION_MIN_MIXIN_6                  7
 #define HF_VERSION_ENFORCE_RCT                  6
 
 #define PER_KB_FEE_QUANTIZATION_DECIMALS        8
+
+#define HASH_OF_HASHES_STEP                     256
+
+#define DEFAULT_TXPOOL_MAX_SIZE                 648000000ull // 3 days at 300000, in bytes
 
 // New constants are intended to go here
 namespace config
@@ -155,9 +159,13 @@ namespace config
 
   uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 26;
   uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 27;
+  uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 28;
   uint16_t const P2P_DEFAULT_PORT = 17369;
   uint16_t const RPC_DEFAULT_PORT = 16035;
+  uint16_t const ZMQ_RPC_DEFAULT_PORT = 16037;
   boost::uuids::uuid const NETWORK_ID = { {
+
+
       0xd7, 0xd7, 0xd7, 0x31, 0x6, 0x19, 0xd7, 0xd7, 0xd7, 0x31, 0x6, 0x19, 0x2b, 0xe3, 0xe, 0xe
     } }; // Bender's nightmare
   std::string const GENESIS_TX = "013c01ff0001bce2c886ba8a02029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101dcd728bd3f8a8631a50a572165f6e40ddf78e871f254d5c70e1d03def3b1af2a";
@@ -167,12 +175,41 @@ namespace config
   {
     uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 53;
     uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 54;
-    uint16_t const P2P_DEFAULT_PORT = 28080;
-    uint16_t const RPC_DEFAULT_PORT = 28081;
+    uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 63;
+    uint16_t const P2P_DEFAULT_PORT = 27080;
+    uint16_t const RPC_DEFAULT_PORT = 27081;
+    uint16_t const ZMQ_RPC_DEFAULT_PORT = 27082;
     boost::uuids::uuid const NETWORK_ID = { {
-        0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x11
-      } }; // Bender's daydream
-    std::string const GENESIS_TX = "013c01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101168d0c4ca86fb55a4cf6a36d31431be1c53a3bd7411bb24e8832410289fa6f3b";
+        0xd7, 0xd7, 0xd7, 0x31, 0x6, 0x19, 0xd7, 0xd7, 0xd7, 0x31, 0x6, 0x19, 0x2b, 0xe3, 0xe, 0xe
+            } }; // Bender's nightmare
+    std::string const GENESIS_TX = "013c01ff0001bce2c886ba8a02029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101dcd728bd3f8a8631a50a572165f6e40ddf78e871f254d5c70e1d03def3b1afd1";
     uint32_t const GENESIS_NONCE = 10001;
   }
+
+  namespace stagenet
+  {
+    uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 24;
+    uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 25;
+    uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 36;
+    uint16_t const P2P_DEFAULT_PORT = 37080;
+    uint16_t const RPC_DEFAULT_PORT = 37081;
+    uint16_t const ZMQ_RPC_DEFAULT_PORT = 37082;
+    boost::uuids::uuid const NETWORK_ID = { {
+        0xd7, 0xd7, 0xd7, 0x31, 0x6, 0x19, 0xd7, 0xd7, 0xd7, 0x31, 0x6, 0x19, 0x2b, 0xe3, 0xe, 0xe
+            } }; // Bender's nightmare
+    std::string const GENESIS_TX = "013c01ff0001bce2c886ba8a02029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101dcd728bd3f8a8631a50a572165f6e40ddf78e871f254d5c70e1d03def3b1af3b";
+    uint32_t const GENESIS_NONCE = 10002;
+  }
+}
+
+namespace cryptonote
+{
+  enum network_type : uint8_t
+  {
+    MAINNET = 0,
+    TESTNET,
+    STAGENET,
+    FAKECHAIN,
+    UNDEFINED = 255
+  };
 }
