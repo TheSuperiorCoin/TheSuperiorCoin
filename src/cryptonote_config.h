0fdd5898839472d1f30898ec9657579779fbc74a
nathansenn <nathan.senn90@gmail.com>
2018-10-31 01:02:35 -0500

    live chain test

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -132,7 +132,7 @@
 
 #define ALLOW_DEBUG_COMMANDS
 
-#define CRYPTONOTE_NAME                         "SuperiorCointest"
+#define CRYPTONOTE_NAME                         "SuperiorCoin"
 #define CRYPTONOTE_POOLDATA_FILENAME            "poolstate.bin"
 #define CRYPTONOTE_BLOCKCHAINDATA_FILENAME      "data.mdb"
 #define CRYPTONOTE_BLOCKCHAINDATA_LOCK_FILENAME "lock.mdb"
